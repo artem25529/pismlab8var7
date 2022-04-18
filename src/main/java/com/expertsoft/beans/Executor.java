3399ddf728013e517b14abcff61ba5f9e68d5b7b
Artem <martinovich.artem02@mail.ru>
2022-04-18 23:36:31 +0300

    Initial commit

---
+++ src/main/java/com/expertsoft/beans/Executor.java
@@ -0,0 +1,51 @@
+package com.expertsoft.beans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean(name = "executor", eager = true)
+@RequestScoped
+public class Executor {
+    private double a;
+    private double b;
+    private double c;
+    private double res;
+
+    public String execute() {
+        res = (a + b) * c;
+
+        return "result";
+    }
+
+    public double getA() {
+        return a;
+    }
+
+    public void setA(double a) {
+        this.a = a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public void setB(double b) {
+        this.b = b;
+    }
+
+    public double getC() {
+        return c;
+    }
+
+    public void setC(double c) {
+        this.c = c;
+    }
+
+    public double getRes() {
+        return res;
+    }
+
+    public void setRes(double res) {
+        this.res = res;
+    }
+}
